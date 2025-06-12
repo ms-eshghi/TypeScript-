bab1e71edeb78142d0311a59fbbe591adf9a3a15
Fatemeh Eshghi <157709672+Fatemeh-Eshghi@users.noreply.github.com>
2025-06-12 18:18:56 +0300

    intial TypeScript practice project

---
+++ dist/app.js
@@ -0,0 +1,54 @@
+"use strict";
+console.log("Hello Friends!");
+let Vehicle = {
+    model: "Boring generic vehicle",
+    color: "Red",
+    year: 1993,
+    power: 60
+};
+console.log("Generic Vehicle:", Vehicle);
+let Car = {
+    model: "Ford focus",
+    color: "Green",
+    year: 2016,
+    power: 150,
+    bodyType: "Hatchback",
+    wheelCount: 4
+};
+let Plan = {
+    model: "Boeing 777",
+    color: "White",
+    year: 2020,
+    power: 170000,
+    wingspan: 65
+};
+let Boat = {
+    model: "Bella",
+    color: "Black",
+    year: 2022,
+    power: 100,
+    draft: 0.42
+};
+console.log("Car:", Car);
+console.log("Boat:", Boat);
+console.log("Plan:", Plan);
+class items {
+}
+function add(name) {
+    return `Hello ${name}`;
+}
+class VehicleService {
+    items = [];
+    add(item) {
+        this.items.push(item);
+    }
+    list() {
+        return this.items;
+    }
+}
+const cars = new VehicleService();
+const boats = new VehicleService();
+cars.add(Car);
+boats.add(Boat);
+console.log("List of car", cars.list());
+console.log("List of car", boats.list());
